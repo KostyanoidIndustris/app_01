eeedad732a3c9cff61f203844af176f5c88a1f1d
KostyanoidIndustris <101281240+KostyanoidIndustris@users.noreply.github.com>
2022-08-27 10:57:26 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Импорт всех классов
+from kivy.app import App
+
+
+from turtle import *
+
+class MyApp(App):
+
+
+    t = Turtle()
+    bgcolor("black")
+    t.pencolor("purple")
+    t.speed(0)
+    for i in range(340):
+        t.circle(190 - i, 90)
+        t.left(90)
+        t.circle(190 - i, 90)
+        t.left(18)
+        if i > 190:
+            t.pensize(3)
+
+    mainloop()
+
+# Запуск проекта
+if __name__ == "__main__":
+    MyApp().run()
